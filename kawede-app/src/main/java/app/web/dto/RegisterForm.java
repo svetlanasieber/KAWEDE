5ce67665ee3555d21638bed48ab8998b90ac0f6d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-20 17:16:28 +0100

    Create RegisterForm.java

---
+++ kawede-app/src/main/java/app/web/dto/RegisterForm.java
@@ -0,0 +1,26 @@
+public class RegisterForm extends LoginFormDTO{
+
+    private String verifyPassword;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
